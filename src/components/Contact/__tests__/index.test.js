7626657169b5142c37b8512f1023a94ae446b923
Dylan Martin <dylanmartin@MacBook-Air.attlocal.net>
2022-10-31 12:31:41 -0500

    Added rendering and snapshot tests for ContactForm component and updated snapshot tests for Nav component

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('ContactForm component', () => {
+  // Render Test
+  it('renders', () => {
+    render(<ContactForm />);
+  });
+
+  // Snapshot Test
+  it('matches snapshot DOM node structure', () => {
+    // render ContactForm
+    const { asFragment } = render(<ContactForm />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  // Renders h1 tag "Contact Me"
+  it('renders', () => {
+    const { getByTestId } = render(<ContactForm />);
+    expect(getByTestId('h1tag')).toHaveTextContent('Contact me');
+  })
+
+  // Renders button tag "Submit"
+  it('renders', () => {
+    const { getByTestId } = render(<ContactForm />);
+    expect(getByTestId('buttontag')).toHaveTextContent('Submit');
+  })
+})
