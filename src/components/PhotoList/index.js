ef3e576a620a2cac96987110082e916dbd063663
Dylan Martin <dylanmartin@MacBook-Air.attlocal.net>
2022-10-31 13:11:58 -0500

    Added markup for modal and image click handler to load image data into modal markup

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Modal from '../Modal';
 
 function PhotoList({ category }) {
   const [photos] = useState([
@@ -98,20 +98,30 @@ function PhotoList({ category }) {
       category: 'landscape',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
-  ]);
-
+  ])
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function toggleModal(targetImage, index) {
+    setCurrentPhoto({ ...targetImage, index });
+    setIsModalOpen(true);
+  }
 
   return (
-    <div className="flex-row">
-      {currentPhotos.map((image, i) => (
-        <img
-          src={require(`../../assets/small/${category}/${i}.jpg`)}
-          alt={image.name}
-          className="img-thumbnail mx-1"
-          key={image.name}
-        />
-      ))}
+    <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      <div className="flex-row">
+        {currentPhotos.map((image, i) => (
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
+            key={image.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
